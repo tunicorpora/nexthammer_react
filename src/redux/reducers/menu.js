87a95a7bf1bcc67d4ab92dd06a20fac2f5ac2d3e
Juho Härme <juho.harme@gmail.com>
2019-01-14 20:47:14 +0200

    temp

--- src/redux/reducers/menu.js
+++ src/redux/reducers/menu.js
@@ -2,17 +2,16 @@ import { TOGGLE_MENU } from './../actiontypes'
 
 export default function menuReducer(state = {}, action) {
 
-
     const { type, ...menu} = action;
 
     switch(type) {
         case TOGGLE_MENU: {
-            //return Object.assign(
-            //    state,
-            //    {
-            //        [ menu.menuname ]: menu.visibility
-            //    }
-            //);
+            return Object.assign(
+                state,
+                {
+                    [ menu.menuname ]: menu.visibility
+                }
+            );
         }
         default:
             return state
